89f3946b6c2b38e223c3cef03d7144dbd72b2706
sarahwcqz <sarah.wacquiez@gmail.com>
2025-08-04 14:26:45 +0200

    task 8 ; post order traversal of tree

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - goes through a binary tree using post-order traversal
+ * @tree: the tree to go through
+ * @func: the function to apply to the tree
+ * Return: void
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree != NULL && func != NULL)
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
+	}
+}
