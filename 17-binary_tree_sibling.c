8a03ed5e2cb55b1a62dceaddc51d459d7446caf7
sarahwcqz <sarah.wacquiez@gmail.com>
2025-08-05 11:13:47 +0200

    modif on task 17, if no siblings found

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,10 +10,10 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
-	if (node->parent->left == node)
+	if (node->parent->left == node && node->parent->right != NULL)
 		return (node->parent->right);
 
-	if (node->parent->right == node)
+	if (node->parent->right == node && node->parent->left != NULL)
 		return (node->parent->left);
 
 	return (NULL);
