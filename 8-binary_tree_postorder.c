788a7a7bd478db5872b6d5adbbfbbcb8f5d8718a
chermat mustapha <mchermat@outlook.fr>
2025-08-07 21:20:32 +0200

    betty style

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through a binary tree using post-order traversal
+ * binary_tree_postorder -goes through a binary tree using post-order traversal
  * @tree: the tree to go through
  * @func: the function to apply to the tree
  * Return: void
